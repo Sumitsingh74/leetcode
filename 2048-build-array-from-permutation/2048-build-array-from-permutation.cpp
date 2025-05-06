5ce05fabaf09184c6af18cf740e65c53010a8785
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-05-06 18:36:02 +0530

    Time: 0 ms (100%), Space: 20.6 MB (39.42%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) 
+          
             nums[i] += (1024 * (nums[nums[i]] % 1024));
         
         for (int i = 0; i < nums.size(); i++) 
