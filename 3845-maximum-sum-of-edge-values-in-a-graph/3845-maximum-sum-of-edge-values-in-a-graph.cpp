3febc47ed6b1c285b52dfc02a154c39ac890ba9a
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-05-13 17:54:32 +0530

    Time: 109 ms (93.15%), Space: 168.8 MB (91.95%) - LeetHub

--- 3845-maximum-sum-of-edge-values-in-a-graph/3845-maximum-sum-of-edge-values-in-a-graph.cpp
+++ 3845-maximum-sum-of-edge-values-in-a-graph/3845-maximum-sum-of-edge-values-in-a-graph.cpp
@@ -1,88 +1,109 @@
+class DSU{
+  public:
+    vector<int> parent,size,edges;
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n);
+        edges.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+            edges[i] = 0;
+        }
+        
+    }
+    void connect(int u,int v){
+        int a = findParent(u);
+        int b = findParent(v);
+        if(a == b){
+            edges[a]++;
+            return;
+        }
+        if(size[a] > size[b]){
+            parent[b] = a;
+            size[a] += size[b];
+            edges[a] += edges[b] + 1;
+        }else{
+            parent[a] = b;
+            size[b] += size[a];
+            edges[b] += edges[a] + 1;
+        }
+    }
+    int findParent(int u){
+        if(u == parent[u]) return u;
+        return parent[u] = findParent(parent[u]);
+    }
+};
 class Solution {
-    vector<int> p, s;
-    void join(int a, int b){
-        if(s[b] > s[a])swap(a,b);
-        p[a] = b;
-        s[b]+=s[a];
-    }
-    int find(int a){
-        if(p[a]==-1)return a;
-        return p[a] = find(p[a]);
-    }
-    long long cyc(int n, int i){
-        long long sm  = 0;
-        long long cur1 = n-1, cur2 = n-2;
-        sm= n*cur1 + n*cur2;
-        i--;
-        while(i>=2){
-            i-=2;
-            if(i==0){
-                sm+=cur1*cur2;
-            }else if(i==1){
-                sm += cur1*(cur2-1) + cur2*(cur2-1);
-            }else{
-                sm += cur1*(cur2-1) + cur2*(cur2-2);
-            }
-            cur1 = cur2-1;
-            cur2 = cur1-1;
-        }
-        return sm;
-    }
-    long long str(int n, int i){
-        cout<<n<<"  "<<i<<endl;
-        long long sm  = 0;
-        if(i == 1)return 0;
-        if(i==2)return n*(n-1);
-        long long cur1 = n-1, cur2 = n-2;
-        sm= n*cur1 + n*cur2;
-        i--;
-        while(i>=2){
-            i-=2;
-            if(i==0){
-                continue;
-            }else if(i==1){
-                sm += cur1*(cur2-1);
-            }else{
-                sm += cur1*(cur2-1) + cur2*(cur2-2);
-            }
-            cur1 = cur2-1;
-            cur2 = cur1-1;
-        }
-        return sm;
-    }
 public:
     long long maxScore(int n, vector<vector<int>>& edges) {
-        p = vector<int>(n,-1);
-        s = vector<int>(n, 1);
-        int x = n;
-        vector<int> res;
-        vector<int> c;
-        long long ans =0;
-        for(auto i: edges){
-            if(find(i[0]) == find(i[1])){
-                c.push_back(s[find(i[1])]);
-                p[find(i[1])] = -2;
-            }else
-            join(find(i[0]), find(i[1]));
+        // 42 35 30 3 12 8 2 ---> 
+        // 42 30 20 28 6 3 2
+
+        unordered_map<int,bool> vis;
+        vector<vector<int>> adj(n);
+        DSU d(n);
+        for(auto i:edges){
+            int u = i[0];
+            int v = i[1];
+            int a = d.findParent(u);
+            int b = d.findParent(v);
+            
+
+            d.connect(u,v);
+        }
+        vector<int> v1,v2;
+        for(int i=0;i<n;i++){
+            int a = d.parent[i];
+            if(a == i){
+                int x = d.size[i];
+                int y = d.edges[i];
+                if(x == y) v1.push_back(x);
+                else v2.push_back(x);
+            }
+            
+        }
+        sort(v1.begin(),v1.end());
+        sort(v2.rbegin(),v2.rend());
+        long long m = n;
+        long long ans = 0;
+        for (auto i : v1) {
+            if (i == 1) continue;
+            long long x = i;
+            long long y = m - x + 1;
+            long long sum = 0;
+
+            for (long long j = y + 2; j <= m; j += 2)
+                sum += j * (j - 2);
+            for (long long j = y + 3; j <= m; j += 2)
+                sum += j * (j - 2);
+
+            sum += m * (m - 1);
+            sum += y * (y + 1);
+            ans += sum;
+            m -= x;
         }
 
-        sort(c.begin(), c.end(), greater<int>());
-        for(auto i: c){
-            ans+=cyc(n,i);
-            n-=i;
-        }
+        for (auto i : v2) {
+            if (i == 1) continue;
+            long long x = i;
+            long long y = m - x + 1;
+            long long sum = 0;
 
-        c = vector<int>();
-        for(int i=0; i<x; i++){
-            if(p[i]==-1 && s[i]>1)c.push_back(s[i]);
-        }
+            for (long long j = y + 2; j <= m; j += 2)
+                sum += j * (j - 2);
+            for (long long j = y + 3; j <= m; j += 2)
+                sum += j * (j - 2);
 
-        sort(c.begin(), c.end(), greater<int>());
-        for(auto i: c){
-            ans+=str(n,i);
-            n-=i;
+            sum += m * (m - 1);
+            ans += sum;
+            m -= x;
         }
 
         return ans;
+        
     }
 };
+
+// 42 35 12 3 8
+// 42 30 28 6 3
