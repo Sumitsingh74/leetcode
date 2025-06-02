41d480a7f1bb93aaaee754e503be448da454c42b
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-06-02 15:42:21 +0530

    Time: 0 ms (100%), Space: 23.9 MB (25%) - LeetHub

--- 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -2,30 +2,27 @@ typedef __int128 ll;
 
 class Solution {
 public:
-    bool solve(int i, ll prod, ll target, vector<int>& nums) {
-        if (prod > target) return false;
-        if (prod == target) return true;
-        if (i == nums.size()) return false;
-        
-        // Include nums[i]
-        if (solve(i + 1, prod * nums[i], target, nums)) return true;
-
-        // Exclude nums[i]
-        if (solve(i + 1, prod, target, nums)) return true;
-
-        return false;
-    }
-
     bool checkEqualPartitions(vector<int>& nums, long long target) {
         int n = nums.size();
-        ll total = 1;
-        for (int x : nums) {
-            total *= x;
-            if (total > (ll)target * target) return false;
+        ll ans = 1;
+        for (auto it : nums) {
+            ans *= it;
+            if (ans > (ll)target * target) return false;
         }
 
-        if (total != (ll)target * target) return false;
+        if (ans != (ll)target * target) return false;
 
-        return solve(0, 1, target, nums);
+        int maxi = (1 << n);
+        for (int i = 1; i < maxi - 1; i++) {
+            ll sub = 1;
+            for (int j = 0; j < n; j++) {
+                if (i & (1 << j)) {
+                    sub *= nums[j];
+                    if (sub > target) break;
+                }
+            }
+            if (sub == target) return true;
+        }
+        return false;
     }
 };
