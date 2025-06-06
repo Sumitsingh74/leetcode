df961b65ba98bcacf13913ae0bb0aa8443348ef2
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-06-07 01:23:47 +0530

    Time: 418 ms (99.44%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-with cte as 
-    (select product_id, new_price, change_date,
-        rank()over(partition by product_id order by change_date desc) as rk
+with ac as(
+    select product_id, new_price, change_date,
+    rank()over(partition by product_id order by change_date desc) as rk
     from products
-    where date(change_date)<='2019-08-16')
-
-select distinct products.product_id, ifnull(cte.new_price,10) as price
-from products left join cte 
-on products.product_id = cte.product_id and rk=1
+    where date(change_date)<='2019-08-16'
+)
+select distinct p.product_id , ifnull(ac.new_price,10) as price
+from products p left join ac
+on p.product_id = ac.product_id and rk=1
