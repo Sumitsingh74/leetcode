93460da52223f3c4060426a1a9a0bdf69763a684
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-06-25 17:23:27 +0530

    Time: 722 ms (52.71%), Space: 55.9 MB (11.63%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,58 +1,42 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        List<Integer> A1 = new ArrayList<>();
-        List<Integer> A2 = new ArrayList<>();
-        List<Integer> B1 = new ArrayList<>();
-        List<Integer> B2 = new ArrayList<>();
-
-        separate(nums1, A1, A2);
-        separate(nums2, B1, B2);
-
-        long negCount = 1L * A1.size() * B2.size() + 1L * A2.size() * B1.size();
-        int sign = 1;
-
-        if (k > negCount) {
-            k -= negCount;
-        } else {
-            k = negCount - k + 1;
-            sign = -1;
-            List<Integer> temp = B1;
-            B1 = B2;
-            B2 = temp;
-        }
-
-        long l = 0, r = (long) 1e10;
-        while (l < r) {
-            long m = (l + r) / 2;
-            if (numProductNoGreaterThan(A1, B1, m) + numProductNoGreaterThan(A2, B2, m) >= k) {
-                r = m;
+        long left = -10000000000L;
+        long right = 10000000000L;
+        
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) {
+                left = mid + 1;
             } else {
-                l = m + 1;
+                right = mid;
             }
         }
-
-        return sign * l;
+        
+        return left;
     }
-
-    private void separate(int[] nums, List<Integer> A1, List<Integer> A2) {
-        for (int a : nums) {
-            if (a < 0) {
-                A1.add(-a);
-            } else {
-                A2.add(a);
-            }
-        }
-        Collections.reverse(A1);
-    }
-
-    private long numProductNoGreaterThan(List<Integer> A, List<Integer> B, long m) {
+    
+    private long countProducts(int[] nums1, int[] nums2, long target) {
         long count = 0;
-        int j = B.size() - 1;
-        for (int a : A) {
-            while (j >= 0 && 1L * a * B.get(j) > m) {
-                j--;
+        for (int num1 : nums1) {
+            if (num1 == 0) {
+                if (target >= 0) count += nums2.length;
+                continue;
             }
-            count += j + 1;
+            
+            int low = 0, high = nums2.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                long product = (long) num1 * nums2[mid];
+                if (product <= target) {
+                    if (num1 > 0) low = mid + 1;
+                    else high = mid;
+                } else {
+                    if (num1 > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+            
+            count += (num1 > 0) ? low : nums2.length - low;
         }
         return count;
     }
