a6996e92a79c5c5428dcebb72028ec137f6db0d5
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-07-06 11:15:53 +0530

    Time: 3 ms (98.8%), Space: 42.2 MB (61.45%) - LeetHub

--- 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.java
+++ 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.java
@@ -1,19 +1,12 @@
 class Solution {
-    static HashMap<Integer,Integer> map=new HashMap<>();
-	public static int minDays(int n) {
-		if(n==1)return 1;
-		if(n<=3)return 2;
-		if(map.containsKey(n))return map.get(n);
-		int ans=(int)1e7;
-		if(n%2==0)ans=Math.min(ans,1+minDays(n/2));
-		if(n%3==0)ans=Math.min(ans,1+minDays(n/3));
-		int d=n-1;
-		if(d%2==0)ans=Math.min(ans,2+minDays(d/2));
-		if(d%3==0)ans=Math.min(ans,2+minDays(d/3));
-		d=n-2;
-		if(d%2==0)ans=Math.min(ans,3+minDays(d/2));
-		if(d%3==0)ans=Math.min(ans,3+minDays(d/3));
-		map.put(n,ans);
-		return ans;
-	}
+    private HashMap<Integer,Integer> mp = new HashMap<>();
+    public int minDays(int n) {
+        if(n <= 2)
+            return n;
+        if(mp.containsKey(n))
+            return mp.get(n);
+        
+        mp.put(n, 1 + Math.min(n % 2 + minDays(n/2), n % 3 + minDays(n/3)));
+        return mp.get(n);
+    }
 }
