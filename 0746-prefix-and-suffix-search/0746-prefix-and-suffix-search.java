93072bec35f8f811309280dbfb7fa4d844aff90a
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-07-15 23:55:49 +0530

    Time: 323 ms (64.61%), Space: 126.1 MB (38.84%) - LeetHub

--- 0746-prefix-and-suffix-search/0746-prefix-and-suffix-search.java
+++ 0746-prefix-and-suffix-search/0746-prefix-and-suffix-search.java
@@ -1,68 +1,61 @@
-class TrieNode {
-    public Map<Character, TrieNode> children;
-    public Set<String> words;
-    public TrieNode() {
-        children = new HashMap<>();
-        words = new HashSet<>();
-    }
-}
+
 class WordFilter {
-    private TrieNode rootP, rootS; 
-    private Map<String, Integer> indexes; 
-    
+    static final int numchars = 27; // 26 for a-z and 1 for '{'
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[numchars];
+        int weight;
+
+        TrieNode() {
+            for(int i = 0; i < numchars; i++) {
+                children[i] = null;
+            }
+        }
+    };
+
+    TrieNode root;
+
+    private void insert(String key, int weight) {
+        TrieNode curr = root;
+        for(char ch : key.toCharArray()) {
+            int index = ch == '{' ? 26 : ch - 'a';
+            if(curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+            curr = curr.children[index];
+            curr.weight = weight;
+        }
+    }
+
     public WordFilter(String[] words) {
-        rootP = new TrieNode();
-        rootS = new TrieNode();
-        indexes = new HashMap<>();
-        for (int i=0; i<words.length; i++) {
-            insert(words[i]);
-            indexes.put(words[i], i);
+        root = new TrieNode();
+        for(int weight = 0; weight < words.length; weight++) {
+            String word = words[weight];
+
+            for(int i = 0; i < word.length(); i++) {
+                // Suffix from i to end + '{' + full word
+                insert(word.substring(i) + '{' + word, weight);
+            }
+            insert("{" + word, weight); //(edge case: "" suffix): (prefix = "")
         }
     }
-    
-    private void insert(String word) {
-        TrieNode ptrP = rootP, ptrS = rootS;
-        for (int i=0; i<word.length(); i++) {
-            // insert as prefixes
-            char c = word.charAt(i);
-            if (!ptrP.children.containsKey(c))
-                ptrP.children.put(c, new TrieNode());
-            ptrP = ptrP.children.get(c);
-            ptrP.words.add(word);
-            
-            // insert as suffixes
-            c = word.charAt(word.length()-1-i);
-            if (!ptrS.children.containsKey(c))
-                ptrS.children.put(c, new TrieNode());
-            ptrS = ptrS.children.get(c);
-            ptrS.words.add(word);
+
+    public int f(String pref, String suff) {
+        String key = suff + '{' + pref;
+        TrieNode curr = root;
+        for(char ch : key.toCharArray()) {
+            int index = ch == '{' ? 26 : ch - 'a';
+            if(curr.children[index] == null) {
+                return -1;
+            }
+            curr = curr.children[index];
         }
-    }
-    
-    public int f(String prefix, String suffix) {
-        TrieNode ptrP = rootP, ptrS = rootS;
-        
-        // get all words with prefix
-        for (int i=0; i<prefix.length(); i++) {
-            char c = prefix.charAt(i);
-            if (!ptrP.children.containsKey(c)) return -1;
-            ptrP = ptrP.children.get(c);            
-        }
-        Set<String> prefixes = ptrP.words;
-        
-        // get all words with suffix
-        for (int i=0; i<suffix.length(); i++) {
-            char c = suffix.charAt(suffix.length()-1-i);
-            if (!ptrS.children.containsKey(c)) return -1;
-            ptrS = ptrS.children.get(c);            
-        }
-        Set<String> suffixes = ptrS.words;
-        
-        int index = -1;
-        for (String word: prefixes) 
-            if (suffixes.contains(word))
-                 index = Math.max(index, indexes.get(word));
-        
-        return index;
+        return curr.weight;
     }
 }
+
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter obj = new WordFilter(words);
+ * int param_1 = obj.f(pref,suff);
+ */
