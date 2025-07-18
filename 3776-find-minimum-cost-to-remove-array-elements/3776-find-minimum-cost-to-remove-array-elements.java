e7df81b3fde3bcf8175a3d8078922018a8d64124
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-07-19 01:53:47 +0530

    Time: 115 ms (58.53%), Space: 55.1 MB (81.57%) - LeetHub

---
+++ 3776-find-minimum-cost-to-remove-array-elements/3776-find-minimum-cost-to-remove-array-elements.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int cal(int ext, int ind, int[] nums, int[][] dp) {
+        // Single element left
+        if (ind == nums.length) return nums[ext];
+
+        // 2 Elements left
+        if (ind == nums.length - 1) return Math.max(nums[ext], nums[ind]);
+
+        if (dp[ind][ext] != -1) return dp[ind][ext];
+        int f = Math.max(nums[ind], nums[ind + 1]) + cal(ext, ind + 2, nums, dp);
+        int s = Math.max(nums[ext], nums[ind + 1]) + cal(ind, ind + 2, nums, dp);
+        int t = Math.max(nums[ext], nums[ind]) + cal(ind + 1, ind + 2, nums, dp);
+        return dp[ind][ext] = Math.min(f, Math.min(s, t));
+    }
+
+    public int minCost(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return cal(0, 1, nums, dp);
+    }
+}
