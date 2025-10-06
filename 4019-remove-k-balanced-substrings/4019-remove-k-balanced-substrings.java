625bedc38e019c1adf09f87004a7658c7e75787b
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-10-06 15:06:22 +0530

    Time: 164 ms (100%), Space: 47.3 MB (49.36%) - LeetHub

--- 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.java
+++ 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.java
@@ -1,43 +1,54 @@
 class Solution {
-    public String removeSubstring(String s, int k) {
-        Stack<Pair> st = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (st.isEmpty() || st.peek().ch != c) {
-                st.push(new Pair(c, 1));
-            } else {
-                st.peek().count++;
-            }
-            if (st.size() >= 2) {
-                Pair top = st.pop();
-                Pair prev = st.pop();
-
-                if (prev.ch == '(' && prev.count >= k && top.ch == ')' && top.count >= k) {
-                    prev.count -= k;
-                    top.count -= k;
-
-                    if (prev.count > 0) st.push(prev);
-                    if (top.count > 0) st.push(top);
-                } else {
-                    st.push(prev);
-                    st.push(top);
-                }
-            }
-        }
-        StringBuilder res = new StringBuilder();
-        for (Pair p : st) {
-            for (int i = 0; i < p.count; i++) res.append(p.ch);
-        }
-
-        return res.toString();
+    static class pe { int a;char c;pe(int x, char c){this.a=x;this.c=c;}}
+   public static String removeSubstring(String s, int k) {
+			String str="";
+			Stack<pe> st=new Stack<pe>();
+			for(int i=0;i<s.length();i++){
+				char c=s.charAt(i);
+				if(c=='('){
+					if(st.isEmpty() ||st.peek().c==')'){
+						st.push(new pe(1,c));
+					}else {
+						pe a=st.pop();
+						st.push(new pe(a.a+1,c));
+					}
+				}else {
+					if(st.size()==0){
+						st.push(new pe(1,c));
+					}else if(st.peek().c=='('){
+						if(k==1){
+							pe a=st.pop();
+							if(a.a>1){
+								st.push(new pe(a.a-1,'('));
+							}
+						}else {
+							st.push(new pe(1,c));
+						}
+					} else {
+						pe t=st.pop();
+						pe b=new pe(t.a+1,c);
+						if(st.size()>0 &&st.peek().c=='('&&st.peek().a>=k&&b.a>=k){
+							pe d=st.pop();
+							if(d.a>k){
+								st.push(new pe(d.a-k,'('));
+							}
+							if(b.a>k){
+								st.push(new pe(b.a-k,')'));
+							}
+						}else st.push(b);
+					}
+				}
+			}
+			StringBuilder sb = new StringBuilder();
+while (!st.isEmpty()) {
+    pe a = st.pop();
+    for (int i = 0; i < a.a; i++) {
+        sb.append(a.c);
     }
+}
+return sb.reverse().toString();
 
-    public class Pair {
-        char ch;
-        int count;
-        Pair(char ch, int count) {
-            this.ch = ch;
-            this.count = count;
-        }
-    }
+            // if(str.length()==0)return new String("");
+			// return str;
+		}
 }
