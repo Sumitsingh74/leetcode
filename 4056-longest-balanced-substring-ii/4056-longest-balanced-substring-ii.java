927e1fafcbe5f4ef04ea4b2647ca7b29021f8412
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-10-12 10:48:40 +0530

    Time: 402 ms (100%), Space: 66.4 MB (100%) - LeetHub

--- 4056-longest-balanced-substring-ii/4056-longest-balanced-substring-ii.java
+++ 4056-longest-balanced-substring-ii/4056-longest-balanced-substring-ii.java
@@ -1,48 +1,60 @@
-import java.util.*;
-
 class Solution {
-    private int n; 
-    private long key(long[] cnt) {
-        return cnt[0] + cnt[1] * (n + 1L) + cnt[2] * (n + 1L) * (n + 1L);
-    }
-
-    public int longestBalanced(String s) {
-        n = s.length();
-        long[] cnt = new long[3];      
-        long[] cur = new long[3];
-        int ans = 0; 
-        Map<Long, Integer>[] mp = new HashMap[8];
-        for (int j = 0; j < 8; j++) {
-            mp[j] = new HashMap<>();
-            mp[j].put(0L, -1);          
+    public static long hashcode(int[]a){
+            return (long) ((long)a[0]*1e12+a[1]*1e6+a[2]);
         }
-
-        for (int i = 0; i < n; i++) {
-            int x = s.charAt(i) - 'a';
-            cnt[x]++;
- 
-            for (int m = 7; m > 0; m--) {
-                long mind = n;
-                for (int b = 0; b < 3; b++) {
-                    int bit = 1 << b;
-                    if ((bit & m) != 0) mind = Math.min(mind, cnt[b]);
-                    cur[b] = cnt[b];
-                }
-
-                for (int b = 0; b < 3; b++) {
-                    int bit = 1 << b;
-                    if ((bit & m) != 0) cur[b] -= mind;
-                }
-
-                long k = key(cur);
-                if (mp[m].containsKey(k)) {
-                    ans = Math.max(ans, i - mp[m].get(k));
-                } else {
-                    mp[m].put(k, i);
+        public static int ch(String str,int c){
+            int ans=0;
+            int n=str.length();int[]arr=new int[n];
+            HashSet<Character> set1=new HashSet<>();
+            for(int i=0;i<n;i++){
+                if(str.charAt(i)-'a'==c)arr[i]= (int) 1e8;
+                else {
+                    if(set1.size()==0){
+                        set1.add(str.charAt(i));
+                        arr[i]=1;
+                    }else {
+                        if(set1.contains(str.charAt(i)))arr[i]=1;
+                        else arr[i]=-1;
+                    }
                 }
             }
+            long sum=0;
+            HashMap<Long,Integer> map=new HashMap<>();map.put(0L,-1);
+            for(int i=0;i<n;i++){
+                sum+=arr[i];
+                if(map.containsKey(sum))ans=Math.max(ans,i-map.get(sum));
+                else map.put(sum,i);
+            }
+            return ans;
         }
+        public static int longestBalanced(String str) {
+            int ans=0;
+            HashMap<Long,Integer> map=new HashMap<>();
+            int[]c=new int[3];
+            map.put(hashcode(c),-1);
+            for(int i=0;i<str.length();i++){
+                c[str.charAt(i)-'a']++;
+                int[]a=new int[3];int min=Math.min(c[0],Math.min(c[1],c[2]));
+                if(min==0&&(c[0]==c[1]||c[0]==c[2]||c[1]==c[2]))ans=Math.max(ans,i+1);
+                a[0]=c[0]-min;a[1]=c[1]-min;a[2]=c[2]-min;
+//                dbg.print(a,"  ",map,"  ");
+                if(map.containsKey(hashcode(a))){
+                    ans=Math.max(i-map.get(hashcode(a)),ans);
+                }
+                else map.put(hashcode(a),i);
+            }
+            int i=0;
+            while(i<str.length()){
+                int cou=1;int j=i+1;
+                while(j<str.length()&&str.charAt(j)==str.charAt(i)){
+                    cou++;j++;
+                }
+                ans=Math.max(ans,cou);
+                i=j;
+            }
+            ans=Math.max(ans,ch(str,0));
+            ans=Math.max(ans,ch(str,1));ans=Math.max(ans,ch(str,2));
 
-        return ans;
-    }
+            return ans;
+        }
 }
